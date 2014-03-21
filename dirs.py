86dd0ecea88a9f03909573da2e1207cb6627b6bf
Jeremy Miller <jmiller@jm-VirtualBox.(none)>
2014-03-21 07:53:25 -0400

    updates

--- dirs.py
+++ dirs.py
@@ -4,11 +4,13 @@ import shutil
 import re
 import os
 
-for root, dirs, files in os.walk(".", topdown=False):
+wheretostart="./node_modules/grunt-cli/node_modules"
+
+for root, dirs, files in os.walk(wheretostart, topdown=False):
     for name in dirs:
 	line = os.path.join(root, name) 
 	matchObj = re.match( r'.*/node_modules/[^/\.]*?$', line, re.M|re.I)
 	if matchObj:
-		if (line.count("node_modules") > 1):
-			print matchObj.group()
-			shutil.move(line,'./node_modules')
+		if (line.count("node_modules") > 2):
+			print "Moving: " + matchObj.group()
+			shutil.move(line,wheretostart)
